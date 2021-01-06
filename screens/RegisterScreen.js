ad8f074ec718899064daec711172700f96674d25
Zubair Rauf <zubair@zubairrauf.com>
2021-01-06 19:47:17 +0100

    added asyncStorage for name and email

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import AppButton from '../components/AppButton'
 import AppText from '../components/AppText'
@@ -12,16 +13,50 @@ function RegisterScreen(props) {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     
+    //Storing data in asyncStorage
+    const storeData = async (value) => {
+        try {
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem('kivName', jsonValue)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    //Reading data from asycStorage
+    //TODO: Move this function to the dashboard screen to show the name
+    // const getData = async () => {
+    //     try {
+    //         const jsonValue = await AsyncStorage.getItem('kivName')
+    //         if(jsonValue !== null) console.log('Data from local storage: ', JSON.parse(jsonValue).name)
+    //         else console.log('No value in local storage')
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+    // }
+
   return (
     <Screen style={styles.container}>
         <Eclips/>
         <AppText style={styles.heading}>Velkommen om bord</AppText>
         <AppText>Vennligst registrer deg</AppText>
         <Image source={require('../assets/images/kid-with-car.png')} style={styles.image}/>
-        <AppTextInput icon="account" placeholder="Navn"/>
-        <AppTextInput icon="email" placeholder="E-post adresse"/>
+        <AppTextInput 
+            icon="account" 
+            placeholder="Navn" value={name}
+            onChangeText={text => setName(text)}
+        />
+        <AppTextInput 
+            icon="email" 
+            placeholder="E-post adresse"
+            value={email}
+            onChangeText={text => setEmail(text)}
+        />
         <View style={styles.button} >
-            <AppButton title="Registrer" onPress={() => console.log('Register pressed')}/>
+            <AppButton 
+                title="Registrer" 
+                onPress={() => storeData({name, email})}
+            />
         </View>
     </Screen>
   );
