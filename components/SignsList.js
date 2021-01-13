b803e839d78be34e4a613afb83cd45e5998023f6
Zubair Rauf <zubair@zubairrauf.com>
2021-01-13 20:10:32 +0100

    Moved the getSigns function to MySignsContext so the saved signs get loaded on the app start

--- components/SignsList.js
+++ components/SignsList.js
@@ -6,12 +6,6 @@ import { signsData } from '../data/signsData'
 import AppListItem from '../components/AppListItem'
 
 function SignsList({ slug, mySigns, setMySigns }) {
-
-    //Get the signs from AsyncStorage on load
-    useEffect(() => {
-        getSigns()
-    }, [])
-
     //Save Signs in AsynStorage on state change
     useEffect(() => {
         console.log('My Signs', mySigns)
@@ -23,24 +17,6 @@ function SignsList({ slug, mySigns, setMySigns }) {
         try {
             const jsonValue = JSON.stringify(value)
             await AsyncStorage.setItem('kivMySigns', jsonValue)
-        } catch (error) {
-            console.error(error)
-        }
-    }
-
-    //Read mySigns from AsyncStorage and update the storeSigns array
-    const getSigns = async () => {
-        try {
-            const jsonValue = await AsyncStorage.getItem('kivMySigns')
-            const parsedValue = jsonValue !== null ? JSON.parse(jsonValue) : null
-            if(parsedValue !== null ) {
-                console.log('parsedValue not null', parsedValue)
-                setMySigns(parsedValue) //This creates error when parsedValue is null
-            }
-            else {
-                console.log('parsedvalue null', parsedValue)
-            }
-
         } catch (error) {
             console.error(error)
         }
--- context/MySignsContext.js
+++ context/MySignsContext.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const MySignsContext = React.createContext()
 
 const MySignsContextProvider = ({ children }) => {
     const [ mySigns, setMySigns ] = useState([])
-    
+    //Get the signs from AsyncStorage on load
+    useEffect(() => {
+        getSigns()
+    }, [])
+    const getSigns = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('kivMySigns')
+            const parsedValue = jsonValue !== null ? JSON.parse(jsonValue) : null
+            if(parsedValue !== null ) {
+                console.log('parsedValue not null', parsedValue)
+                setMySigns(parsedValue) //This creates error when parsedValue is null
+            }
+            else {
+                console.log('parsedvalue null', parsedValue)
+            }
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <MySignsContext.Provider value={{ mySigns, setMySigns }}>
             {children}
