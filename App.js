55435b7f30519bea88c492472f933fd1bd520e2c
Zubair Rauf <zubair@zubairrauf.com>
2021-02-20 16:29:25 +0100

    Nested AppNavigator into AuthNavigator in App.js

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
       <MySignsContextProvider>
-        <AppNavigator />
+        <AuthNavigator />
       </MySignsContextProvider>
     </NavigationContainer>
   );
--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -4,13 +4,14 @@ import { createStackNavigator } from '@react-navigation/stack'
 import SplashScreen from '../screens/SplashScreen'
 import RegisterScreen from '../screens/RegisterScreen';
 import DashboardScreen from '../screens/DashboardScreen';
+import AppNavigator from './AppNavigator';
 
 const Stack = createStackNavigator();
 const AuthNavigator = () => (
     <Stack.Navigator>
         <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Dashboard" component={AppNavigator} options={{ headerShown: false }}/>
     </Stack.Navigator>
 )
 
