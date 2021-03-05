6dfca406c1bc313db5991f14ab81cfd3ef157f72
Zubair <zubairrr@gmail.com>
2021-03-05 21:43:09 +0100

    added message for empty mySigns list

--- screens/MySignsScreen.js
+++ screens/MySignsScreen.js
@@ -1,32 +1,59 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
-import {MySignsContext} from '../context/MySignsContext'
-import Eclips from '../components/Eclips'
-import Screen from '../components/Screen'
-import SignsList from '../components/SignsList'
-import AppHeader from '../components/AppHeader'
+import { MySignsContext } from "../context/MySignsContext";
+import Eclips from "../components/Eclips";
+import Screen from "../components/Screen";
+import SignsList from "../components/SignsList";
+import AppHeader from "../components/AppHeader";
+import AppText from "../components/AppText";
 
 function SignsScreen() {
   return (
     <Screen style={styles.container}>
-        <Eclips />
-        <AppHeader title='Mine skilt' />
-        <MySignsContext.Consumer>
-          {({mySigns, setMySigns}) => (
-              <SignsList 
-                slug='mySigns'
+      <Eclips />
+      <AppHeader title="Mine skilt" />
+      <MySignsContext.Consumer>
+        {({ mySigns, setMySigns }) => {
+          if (mySigns.length > 0) {
+            return (
+              <SignsList
+                slug="mySigns"
                 mySigns={mySigns} //comming from context
                 setMySigns={setMySigns} //comming from context
               />
-          )}
-        </MySignsContext.Consumer>
+            );
+          } else {
+            return (
+              <View style={styles.container}>
+                <AppText style={styles.title}>Ingen skilt lagret.</AppText>
+                <AppText style={styles.description}>
+                  Her dukker opp en liste av skilt som du lagrer ved å trykke på
+                  + ikonen.
+                </AppText>
+              </View>
+            );
+          }
+        }}
+      </MySignsContext.Consumer>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "700",
+  },
+  description: {
+    fontSize: 16,
+    textAlign: "center",
+  },
 });
 
 export default SignsScreen;
