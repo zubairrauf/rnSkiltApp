2e92bddba56ea4c5fc79b325d8e1bc86e0afe9e8
Zubair <zubairrr@gmail.com>
2021-03-05 21:12:51 +0100

    Added badge to Mine skilt and added context provider to the AppNavigation

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
-import AppNavigator from './navigation/AppNavigator';
-import AuthNavigator from './navigation/AuthNavigator'
-import { MySignsContextProvider } from './context/MySignsContext'
-import navigationTheme from './navigation/navigationTheme';
-
+import AppNavigator from "./navigation/AppNavigator";
+import AuthNavigator from "./navigation/AuthNavigator";
+import { MySignsContextProvider } from "./context/MySignsContext";
+import navigationTheme from "./navigation/navigationTheme";
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <MySignsContextProvider>
+    <MySignsContextProvider>
+      <NavigationContainer theme={navigationTheme}>
         <AuthNavigator />
-      </MySignsContextProvider>
-    </NavigationContainer>
+      </NavigationContainer>
+    </MySignsContextProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,48 +1,68 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import HomeNavigator from './HomeNavigator'
-import DashboardScreen from '../screens/DashboardScreen';
-import SignsScreen from '../screens/SignsScreen';
-import MySignsScreen from '../screens/MySignsScreen';
-import QuizScreen from '../screens/QuizScreen';
-import colors from '../config/colors';
+import { MySignsContext } from "../context/MySignsContext";
+import HomeNavigator from "./HomeNavigator";
+import DashboardScreen from "../screens/DashboardScreen";
+import SignsScreen from "../screens/SignsScreen";
+import MySignsScreen from "../screens/MySignsScreen";
+import QuizScreen from "../screens/QuizScreen";
+import colors from "../config/colors";
 
 //Tab Navigation
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 const AppNavigator = () => (
-  <Tab.Navigator
-    tabBarOptions={{
-      activeBackgroundColor: colors.secondary,
-      activeTintColor: colors.dark,
-      inactiveBackgroundColor: colors.light,
-      inactiveTintColor: colors.dark,
-      labelStyle: { fontSize: 12, fontWeight: '700' }
-    }}
-  >
-    <Tab.Screen 
-      name="Home" 
-      component={HomeNavigator}
-      options={{ 
-        tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="home" size={size} color={color}/>
-      }}
-    />
-    <Tab.Screen 
-      name="Mine skilt" 
-      component={MySignsScreen}
-      options={{ 
-        tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="sign-direction" size={size} color={color}/>
-      }}
-    />
-    <Tab.Screen 
-      name="Quiz" 
-      component={QuizScreen}
-      options={{ 
-        tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="arrow-decision" size={size} color={color}/>
-      }}
-    />
-  </Tab.Navigator>
-  )
+  <MySignsContext.Consumer>
+    {({ mySigns }) => (
+      <Tab.Navigator
+        tabBarOptions={{
+          activeBackgroundColor: colors.secondary,
+          activeTintColor: colors.dark,
+          inactiveBackgroundColor: colors.white,
+          inactiveTintColor: colors.dark,
+          labelStyle: { fontSize: 12, fontWeight: "700" },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeNavigator}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialCommunityIcons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Mine skilt"
+          component={MySignsScreen}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialCommunityIcons
+                name="sign-direction"
+                size={size}
+                color={color}
+              />
+            ),
+            tabBarBadge: mySigns.length,
+          }}
+        />
+        <Tab.Screen
+          name="Quiz"
+          component={QuizScreen}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialCommunityIcons
+                name="arrow-decision"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    )}
+  </MySignsContext.Consumer>
+);
 
-  export default AppNavigator
+export default AppNavigator;
