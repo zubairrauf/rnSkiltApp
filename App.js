3e4577304d3f4578ad13d7b767b9d8c8c1155335
Zubair <zubairrr@gmail.com>
2021-03-12 19:18:20 +0100

    Fixing merge conflict

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-=======
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-
-import AppNavigator from './navigation/AppNavigator';
-import AuthNavigator from './navigation/AuthNavigator'
-import { MySignsContextProvider } from './context/MySignsContext'
-import navigationTheme from './navigation/navigationTheme';
-import FlashcardsDeck from './components/FlashcardsDeck'
->>>>>>> c06d3286d6c9f9431dcd3aca5cd949508efb70b6
 
 import AuthNavigator from "./navigation/AuthNavigator";
 import { MySignsContextProvider } from "./context/MySignsContext";
@@ -20,20 +8,11 @@ import navigationTheme from "./navigation/navigationTheme";
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <MySignsContextProvider>
       <NavigationContainer theme={navigationTheme}>
         <AuthNavigator />
       </NavigationContainer>
     </MySignsContextProvider>
-=======
-    // <NavigationContainer theme={navigationTheme}>
-    //   <MySignsContextProvider>
-    //     <AuthNavigator />
-    //   </MySignsContextProvider>
-    // </NavigationContainer>
-    <FlashcardsDeck />
->>>>>>> c06d3286d6c9f9431dcd3aca5cd949508efb70b6
   );
 }
 
