7c63bbd86ce57e3b7a89e84e867f58e589d45c83
Zubair <zubairrr@gmail.com>
2021-04-23 18:11:13 +0200

    corrected folder name and added name check to load correct navigator on app load

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import AuthNavigator from "./navigation/AuthNavigator";
+import AppNavigator from "./navigation/AppNavigator";
 import { MySignsContextProvider } from "./context/MySignsContext";
 import navigationTheme from "./navigation/navigationTheme";
 
 export default function App() {
+  const [name, setName] = useState();
+  //Reading the name from AsynStorage and updating the name state.
+  const getName = async (key) => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(key);
+      const value = JSON.parse(jsonValue);
+      setName(value.name);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //Run the getName on component load
+  useEffect(() => {
+    getName("kivName");
+  }, []);
+
   return (
     <MySignsContextProvider>
       <NavigationContainer theme={navigationTheme}>
-        <AuthNavigator />
+        {name ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </MySignsContextProvider>
   );
 }
-
